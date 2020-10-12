97b6a7abd21f63458f95ebbbdc94cb7954c2157a
neozumm <neozumm@rambler.ru>
2020-10-13 00:07:11 +0300

    fix todos

--- lab1.py
+++ lab1.py
@@ -2,13 +2,14 @@ import typing
 
 
 def get_mod_from_level(level: int):
-    #FIXME проверка уровня
+    if (level < 7) | (level > 17):
+        return None
     mod_bonus_level: float = 0
     if level < 10:
         mod_bonus_level = 0.05
-    elif level >= 10 & level < 13:
+    elif 10 <= level < 13:
         mod_bonus_level = 0.1
-    elif level >= 13 & level < 15:
+    elif 13 <= level < 15:
         mod_bonus_level = 0.15
     elif level >= 15:
         mod_bonus_level = 0.2
@@ -18,7 +19,7 @@ def get_mod_from_level(level: int):
 
 
 def get_mod_from_review(review_score: float):
-    if (review_score < 7) | (review_score > 17): #TODO исправить на границу уровня
+    if (review_score < 0) | (review_score > 5):
         return None
     mod_bonus_review: float = 0
     if (review_score >= 2) & (review_score < 2.5):
@@ -33,19 +34,17 @@ def get_mod_from_review(review_score: float):
         mod_bonus_review = 2
     return mod_bonus_review
 
+quartal = 3
 
-def calculate_bonus(mod_level: float, mod_review: float, salary: int):
-    # TODO сделать входной точкой
+def calculate_bonus (level: int,review: float, salary: int):
+    mod_level = get_mod_from_level(level)
     if (mod_level == None):
-        return "wrong level mod" 
+        return "wrong level mod"
+    mod_review = get_mod_from_review(review)
     if (mod_review == None):
         return "wrong review mod"
-    if (salary >= 75000) & (salary <= 750000):  #TODO квартал
-        bonus = salary * mod_level
+    if (salary >= 75000) & (salary <= 750000):
+        bonus = quartal * salary * mod_level
         return bonus*mod_review
     else:
         return "wrong salary value"
-
-if __name__ == "__main__":
-    mod_lvl = get_mod_from_level(5)
-    print( calculate_bonus(get_mod_from_level(15), get_mod_from_review(5), 750000) )
